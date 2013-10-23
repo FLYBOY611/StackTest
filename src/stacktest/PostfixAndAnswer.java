7a607b7fb27fb06057fcabeb4adb2b7aacf22a43
Richard <Richard@Richard-PC>
2013-10-23 13:42:01 -0400

    Working stack program.

---
+++ src/stacktest/PostfixAndAnswer.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Module Name: PostfixAndAnswer										
+*												
+* Module Description: A class that we use to return two item at the same time.
+* In this case, the postfix and and evaluation of the postfix
+*												
+* @Param:											
+*												
+* @Return:                      									
+*												
+*													
+*******************************************************************************/
+package stacktest;
+
+/**
+ *
+ * @author Richard
+ */
+public class PostfixAndAnswer{
+    public String postfix;
+    public String answer;
+}
